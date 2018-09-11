2980700a6f11572c8ff12a816573693f37c9bf41
zz <zhuguang@ejoy.com>
2018-09-11 21:38:35 +0800

    [ADDED] type propote

---
+++ include/sw/NodeHelper.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace sw
+{
+
+class Node;
+
+class NodeHelper
+{
+public:
+	static void TypePropote(Node& node);
+
+}; // NodeHelper
+
+}
---
+++ source/NodeHelper.cpp
@@ -0,0 +1,36 @@
+#include "sw/NodeHelper.h"
+#include "sw/Node.h"
+
+namespace sw
+{
+
+void NodeHelper::TypePropote(Node& node)
+{
+	auto& imports = node.GetImports();
+	bool empty = true;
+	for (auto& p : imports) {
+		if (!p.conns.empty()) {
+			empty = false;
+			break;
+		}
+	}
+
+	VariableType type;
+	for (auto& p : imports)
+	{
+		auto in_port = p.GetPair(0);
+		if (!in_port) {
+			continue;
+		}
+		type.TypePromoteFrom(in_port->var.Type());
+	}
+
+	for (auto& p : imports) {
+		const_cast<sw::Node::Port&>(p).var.Type().TypePromoteFrom(type);
+	}
+	for (auto& p : node.GetExports()) {
+		const_cast<sw::Node::Port&>(p).var.Type().TypePromoteFrom(type);
+	}
+}
+
+}
