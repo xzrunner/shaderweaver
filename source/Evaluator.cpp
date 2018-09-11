6c86e4e5bff89b228fb6ffcb9b6a19b98bff6fb2
zz <zhuguang@ejoy.com>
2018-09-11 21:38:52 +0800

    [FIXED] type check

--- source/Evaluator.cpp
+++ source/Evaluator.cpp
@@ -97,7 +97,8 @@ void Evaluator::Concatenate(Node::Port& from, Node::Port& to)
 	assert(f_type.interp == VT_INTERP_ANY
 		|| t_type.interp == VT_INTERP_ANY
 		|| f_type.interp == t_type.interp);
-	assert(f_type.precision == t_type.precision);
+	assert(f_type.precision == VT_PRECISION_ANY
+		|| f_type.precision == t_type.precision);
 
 	if (f_type.dim != t_type.dim)
 	{
