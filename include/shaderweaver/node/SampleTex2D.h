31b65ae75de1db81015d85d7fdacdeda9ced6cf7
zz <zhuguang@ejoy.com>
2018-09-28 07:18:23 +0800

    [ADDED] impl output 4 single channels

--- include/shaderweaver/node/SampleTex2D.h
+++ include/shaderweaver/node/SampleTex2D.h
@@ -18,7 +18,11 @@ public:
 			{ t_tex2d, "tex" },
 			{ t_uv, "uv" },
 		}, {
-			{ t_col4 | t_read, "col4" },
+			{ t_col4 | t_read, "rgba" },
+			{ t_flt1 | t_read, "r" },
+			{ t_flt1 | t_read, "g" },
+			{ t_flt1 | t_read, "b" },
+			{ t_flt1 | t_read, "a" },
 		}, {
 		});
 	}
@@ -29,13 +33,39 @@ public:
 		ID_UV,
 	};
 
+	enum OutputID
+	{
+		ID_RGBA = 0,
+		ID_R,
+		ID_G,
+		ID_B,
+		ID_A,
+	};
+
 protected:
 	virtual std::string GetBody() const override
 	{
+		std::string ret;
+
 		// todo: in phong, result will be changed by another one before using
 		// 1. clip phong to small block, or
 		// 2. add special flag to make result read only
-		return "#col4# = texture2D(#tex#, #uv#);\n";
+		ret += "#rgba# = texture2D(#tex#, #uv#);\n";
+
+		if (!m_exports[ID_R].conns.empty()) {
+			ret += "#r# = #rgba#.r;\n";
+		}
+		if (!m_exports[ID_G].conns.empty()) {
+			ret += "#g# = #rgba#.g;\n";
+		}
+		if (!m_exports[ID_B].conns.empty()) {
+			ret += "#b# = #rgba#.b;\n";
+		}
+		if (!m_exports[ID_A].conns.empty()) {
+			ret += "#a# = #rgba#.a;\n";
+		}
+
+		return ret;
 	}
 
 }; // SampleTex2D
