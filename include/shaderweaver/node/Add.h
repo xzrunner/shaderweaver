ff239be709d3243b73c4480e88bfd4589a3bbe8c
zz <zhuguang@ejoy.com>
2019-03-21 07:37:57 +0800

    [ADDED] support extensible input ports

--- include/shaderweaver/node/Add.h
+++ include/shaderweaver/node/Add.h
@@ -31,10 +31,43 @@ public:
 		ID_B,
 	};
 
+    void SetInputPortCount(int count)
+    {
+        if (count <= 2) {
+            return;
+        }
+
+        std::vector<Variable> input;
+        input.resize(count);
+        for (int i = 0; i < count; ++i) {
+            std::string name;
+            name.push_back(static_cast<char>('a' + i));
+            input[i] = Variable(0, name);
+        }
+        InitVariables(input, { { 0, "_out" }, }, {});
+
+        m_dim_group.resize(count + 1);
+        for (int i = 0; i < count; ++i) {
+            m_dim_group[i] = i;
+        }
+        m_dim_group[count] = MAX_IMPORTS_COUNT;
+    }
+
 protected:
 	virtual std::string GetBody() const override
 	{
-		return "#_out# = #a# + #b#;\n";
+        std::string str_sum;
+        for (int i = 0, n = m_imports.size(); i < n; ++i)
+        {
+            str_sum.push_back('#');
+            str_sum.push_back(static_cast<char>('a' + i));
+            str_sum.push_back('#');
+            if (i != n - 1) {
+                str_sum += " + ";
+            }
+        }
+
+        return cpputil::StringHelper::Format("#_out# = %s;\n", str_sum.c_str());
 	}
 
 	RTTR_ENABLE(Node)
